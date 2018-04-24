6f283bf092c2018515d5d3eaf71f3d3d184b0a5b
TechStream <38688193+TechStream@users.noreply.github.com>
2018-04-24 14:55:01 +0500

    File added
    File added with full code

---
+++ fence.ino
@@ -0,0 +1,18 @@
+int sensorPin = A0;
+int BuzPin = 7;
+int value = 0;
+void setup() {
+Serial.begin(9600);
+pinMode(BuzPin,OUTPUT);
+}
+void loop() {  
+value = analogRead (sensorPin);
+Serial.println (value, DEC);
+if(value>200)
+{
+	digitalWrite(BuzPin,HIGH);
+  delay(1000);
+  digitalWrite(BuzPin,LOW);
+}
+delay (50);
+}
